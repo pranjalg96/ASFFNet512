7620a5800ae5fc9cd6ab673262719f804bd0a1af
pranjalg96 <41956641+pranjalg96@users.noreply.github.com>
2023-08-04 11:05:11 +0530

    added SNGAN discrimnator, adv loss and adv train

---
+++ models/SNGAN_disc.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.utils.spectral_norm as SpectralNorm
+
+class SNGANDiscriminator(nn.Module):
+    def __init__(self, in_channels=3, num_features=64):
+        super(SNGANDiscriminator, self).__init__()
+        self.conv1 = SpectralNorm(nn.Conv2d(in_channels, num_features, 4, 2, 1, bias=False))
+        self.conv2 = SpectralNorm(nn.Conv2d(num_features, num_features * 2, 4, 2, 1, bias=False))
+        self.conv3 = SpectralNorm(nn.Conv2d(num_features * 2, num_features * 4, 4, 2, 1, bias=False))
+        self.conv4 = SpectralNorm(nn.Conv2d(num_features * 4, num_features * 8, 4, 2, 1, bias=False))
+        self.conv5 = SpectralNorm(nn.Conv2d(num_features * 8, 1, 4, 1, 0, bias=False))
+        self.global_avg_pool = nn.AdaptiveAvgPool2d(1)
+
+    def forward(self, x):
+        x = F.leaky_relu(self.conv1(x), 0.2)
+        x = F.leaky_relu(self.conv2(x), 0.2)
+        x = F.leaky_relu(self.conv3(x), 0.2)
+        x = F.leaky_relu(self.conv4(x), 0.2)
+        x = self.conv5(x)
+        x = self.global_avg_pool(x)
+        x = x.view(-1)
+        return torch.tanh(x)
--- train.py
+++ train.py
@@ -7,9 +7,11 @@ from torch.utils.data import DataLoader
 
 from utils.train_utils import get_lmarks_from_tensor, generate_lq_point_mask
 from utils.WLS import guidance_selection
-from models.ASFFNet import ASFFNet
-
 from utils.losses import mse_loss, perceptual_loss, style_loss
+
+from models.ASFFNet import ASFFNet
+from models.SNGAN_disc import SNGANDiscriminator
+
 
 from torchvision.transforms.functional import normalize
 import face_alignment
@@ -25,8 +27,10 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Training parameters
 batch_size = 1
-lr = 2e-4
+asff_lr = 2e-4
+disc_lr = 2e-4
 num_epochs = 10
+report_interval = 2
 checkpoint_interval = 5
 
 # Loss coefficients
@@ -44,9 +48,12 @@ train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
 # Define the ASFFNet model
 ASFFNet512 = ASFFNet().to(device).train()
 
+# Define the SNGAN discriminator model (For real images, output should be close to 1. For fake images, output should be close to -1)
+SNGANDisc = SNGANDiscriminator().to(device).train()
+
 # Define optimizers
-asff_opt = torch.optim.Adam(ASFFNet512.parameters(), lr=lr)
-# optimizer_D = optim.Adam(ASFFNet512.parameters(), lr=lr)
+asff_opt = torch.optim.Adam(ASFFNet512.parameters(), lr=asff_lr, betas=(0.5, 0.999))
+sngan_disc_opt = torch.optim.Adam(SNGANDisc.parameters(), lr=disc_lr, betas=(0.5, 0.999))
 
 # Load face detection module
 FaceDetection = face_alignment.FaceAlignment(face_alignment.LandmarksType.TWO_D, device='cuda' if torch.cuda.is_available() else 'cpu')
@@ -76,37 +83,63 @@ for epoch in range(num_epochs):
         lq_image_batch, hq_selected_image, hq_selected_lmark, gt_image_batch, lq_landmarks_mask = lq_image_batch.to(device), hq_selected_image.to(device), hq_selected_lmark.to(device), gt_image_batch.to(device), lq_landmarks_mask.to(device)
         lq_lmarks = torch.from_numpy(lq_lmarks).unsqueeze(0).to(device)
 
-        # Forward pass
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        # Training ASFFNet
+        asff_opt.zero_grad()
+
+        # Forward pass through ASFFNet
         restored_img_batch = ASFFNet512(lq_image_batch, hq_selected_image, lq_landmarks_mask, lq_lmarks, hq_selected_lmark)
 
-        # Compute reconstruction loss
         mse_loss_val = mse_loss(restored_img_batch, gt_image_batch)
         perc_loss_val = perceptual_loss(restored_img_batch, gt_image_batch)
-
+        
+        # Compute reconstruction loss
         reconstruction_loss = lambda_mse * mse_loss_val + lambda_perc * perc_loss_val
 
-        # Compute photo-realistic loss
         style_loss_val = style_loss(restored_img_batch, gt_image_batch)
-#         adv_loss_val = -torch.mean(asffnet(id_imgs, id_imgs, id_landmarks, id_landmarks))
+        fake_logits = SNGANDisc(restored_img_batch)
+        adv_loss_val = -torch.mean(fake_logits)
 
-        photo_realistic_loss = lambda_style * style_loss_val  # + lambda_adv * adv_loss_val
+        # Compute photo-realistic loss
+        photo_realistic_loss = lambda_style * style_loss_val + lambda_adv * adv_loss_val
 
         # Compute the overall loss
-        total_loss = reconstruction_loss + photo_realistic_loss
+        total_asffnet_loss = reconstruction_loss + photo_realistic_loss
 
-        # Backpropagation
-        asff_opt.zero_grad()
-        total_loss.backward()
+        # Backpropagation through ASFFNet
+        total_asffnet_loss.backward()
         asff_opt.step()
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        # Training SNGAN Discriminator
+        sngan_disc_opt.zero_grad()
 
+        # Forward pass through ASFFNet. Detach the tensor after since only discriminator is trained
+        restored_img_batch = ASFFNet512(lq_image_batch, hq_selected_image, lq_landmarks_mask, lq_lmarks, hq_selected_lmark).detach()
+
+        fake_logits = SNGANDisc(restored_img_batch)
+        real_logits = SNGANDisc(gt_image_batch)
+
+        # Hinge version of adversarial loss for discriminator
+        d_loss_real = torch.mean(torch.nn.ReLU()(1.0 - real_logits))
+        d_loss_fake = torch.mean(torch.nn.ReLU()(1.0 + fake_logits))
+        d_total_loss = d_loss_real + d_loss_fake
+
+        # Backpropogation through SNGAN Discriminator
+        d_total_loss.backward()
+        sngan_disc_opt.step()
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         # Print progress
-        if (i + 1) % 2 == 0:
-            print(f"Epoch [{epoch+1}/{num_epochs}] Batch [{i+1}/{len(train_loader)}] Loss: {total_loss.item()} MSE_Loss: {mse_loss_val.item()} Perc_Loss: {perc_loss_val.item()} Style_Loss: {style_loss_val.item()}")
+        if (i + 1) % report_interval == 0:
+            print(f"Epoch [{epoch+1}/{num_epochs}] Batch [{i+1}/{len(train_loader)}] ASFF_Loss: {total_asffnet_loss.item()} (MSE_Loss: {mse_loss_val.item()} Perc_Loss: {perc_loss_val.item()} Style_Loss: {style_loss_val.item()}) Disc_Loss: {d_total_loss.item()}")
 
     # Save model checkpoints
     if (epoch + 1) % checkpoint_interval == 0:
-        checkpoint_path = f"{args.save_dir}/asffnet_epoch_{epoch + 1}.pth"
-        torch.save(ASFFNet512.state_dict(), checkpoint_path)
-        print(f"Saved checkpoint at {checkpoint_path}")
+        checkpoint_path_asff = f"{args.save_dir}/asffnet_adv_epoch_{epoch + 1}.pth"
+        checkpoint_path_disc = f"{args.save_dir}/sngan_disc_epoch_{epoch + 1}.pth"
+
+        torch.save(ASFFNet512.state_dict(), checkpoint_path_asff)
+        torch.save(SNGANDisc.state_dict(), checkpoint_path_disc)
+
+        print(f"Saved checkpoints at {checkpoint_path_asff}, {checkpoint_path_disc}")
 
 print("Training completed.")
